867ded057f6391a2d441c796612eadd0e68f52a1
Aleksander Grzybowski <ifyouseethis@tellmeplease.it>
2019-05-23 17:19:11 +0200

    Empty Spring Boot project

---
+++ src/main/java/pl/kelog/trackingapp/TrackingAppApplication.java
@@ -0,0 +1,13 @@
+package pl.kelog.trackingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingAppApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(TrackingAppApplication.class, args);
+    }
+    
+}
