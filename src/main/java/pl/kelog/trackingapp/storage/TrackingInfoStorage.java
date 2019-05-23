2c2e02cef7daa869005ba7e8d8bdd8c47c2e5bac
Aleksander Grzybowski <kelog@orange.pl>
2019-05-23 18:14:32 +0200

    Checking if storage is writable

--- src/main/java/pl/kelog/trackingapp/storage/TrackingInfoStorage.java
+++ src/main/java/pl/kelog/trackingapp/storage/TrackingInfoStorage.java
@@ -3,10 +3,13 @@ package pl.kelog.trackingapp.storage;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -23,7 +26,7 @@ public class TrackingInfoStorage {
     public TrackingInfoStorage(
             ObjectMapper mapper,
             @Value("${storagePath:null}") String storagePath
-    ) {
+    ) throws IOException {
         this.mapper = mapper;
         this.storagePath = storagePath;
         
@@ -44,7 +47,15 @@ public class TrackingInfoStorage {
         return FORMATTER.format(timestamp) + ".json";
     }
     
-    private void ensureStorageIsWritable() {
-        // TODO
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    private void ensureStorageIsWritable() throws IOException {
+        log.info("Checking if storage is writable...");
+        
+        String testFilename = "" + System.currentTimeMillis();
+        File testFile = Paths.get(storagePath, testFilename).toFile();
+        FileUtils.write(testFile, "testing", Charset.defaultCharset());
+        testFile.delete();
+        
+        log.info("Storage is functioning properly.");
     }
 }
